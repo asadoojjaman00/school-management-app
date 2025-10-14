f97da00e66362006cdc1ad689d1a143fd42ca87a
Asadoojjaman <asadoojjaman.cse@gmail.com>
2025-10-15 01:54:52 +0600

    updated

---
+++ accounts/managers.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _ 
+from django.db import models
+
+class UserManager(BaseUserManager):
+    
+
+    def _create_user(self, email, full_name, password, **extra_fields):
+
+        if not email:
+            raise ValueError("user must have an email")
+      
+        if not full_name:
+            raise ValueError("user must have an full_name")
+        
+
+        email = self.normalize_email(email)
+
+        username = email.split('@')[0].lower().replace(' ', '_')[:40]
+        extra_fields.setdefault('username', username)
+
+        user = self.model(
+            username=username,
+            full_name=full_name,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, email, full_name=None, password=None, **extra_fields):
+
+        extra_fields.setdefault("is_admin", False)
+        extra_fields.setdefault("is_superuser", False)
+        extra_fields.setdefault("is_active", False)
+
+        return self._create_user(email, full_name, password, **extra_fields)
+    
+    def create_superuser(self, email, full_name= None, password=None, **extra_fields):
+
+        extra_fields.setdefault("is_admin", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_admin") is not True:
+            raise ValueError("superuser must have is_admin = True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("superuser must have is_superuser = True ")
+        
+        return self._create_user(email, full_name, password, **extra_fields)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,87 @@
 from django.contrib.auth.models import AbstractUser , PermissionsMixin
-from django.contrib.auth.base_user import BaseUserManager 
+from .managers import UserManager
+from django.db import models
+from django.utils.translation import gettext_lazy as _ 
 
+
+
+class User(AbstractUser, PermissionsMixin):
+
+    full_name = models.CharField(max_length=255)
+
+    username = models.CharField(
+        _("username"),
+        max_length=40,
+        unique=True,
+        db_index=True,
+        error_messages={
+            "unique": _("A user with that username already exist")
+        }
+    )
+
+    email = models.EmailField(
+        max_length=250,
+        unique=True,
+        error_messages={
+            "unique": _("A user with that email already exist ")
+        }
+    )
+    date_of_birth = models.DateField(null=True, blank=True)
+    GENDER_CHOICE= [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other')
+    ]
+
+    gender = models.CharField(
+        max_length=1,
+        choices= GENDER_CHOICE,
+        null= True,
+        blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_at = models.DateTimeField(auto_now_add=False ,auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name']
+
+    def __str__(self):
+        return self.email
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['username']),
+        ] 
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
+    
+
+class UserProfile(User):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    DEPARTMENT_CHOICE=[
+        ('cst', 'CST'),
+        ('civil', 'CIVIL'),
+        ('eee', 'EEE'),
+        ('aidt', 'AIDT'),
+    ]
+
+    department = models.CharField(
+        max_length=6,
+        choices=DEPARTMENT_CHOICE,
+        null=True,
+        blank=True
+    )
+    board_roll = models.CharField(max_length=6, unique=True, null=True, blank=False)
+
+    
